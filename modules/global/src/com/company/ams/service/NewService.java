9b4409e29f5c174364f4cfb1d4b3acac916338be
Elangovan22 <elangshoba@gmail.com>
2017-04-04 01:54:39 +0530

    Full-Module

---
+++ modules/global/src/com/company/ams/service/NewService.java
@@ -0,0 +1,21 @@
+package com.company.ams.service;
+import com.haulmont.cuba.core.global.View;
+import com.company.ams.entity.Asset;
+import com.company.ams.entity.NewAsset;
+import com.company.ams.entity.Instance;
+import com.company.ams.entity.AssetAllocated;
+import java.util.Collection;
+
+public interface NewService {
+    String NAME = "ams_NewService";
+
+    Collection<Instance> createInstances(NewAsset asset, Integer amount,String vv,Integer tm,String user);
+
+   Collection<Instance> calculate(Instance insta,int assetid,int totalnumber);
+
+public Collection<NewAsset> updatequantity(int assetid,int totalnumber);
+
+ Collection<AssetAllocated> allocated(Instance asset);
+
+
+}
